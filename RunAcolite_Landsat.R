86ed24d0f1d0087c3f536e0e7dd1a7eaaad39c20
ecormier123 <cormieremily@hotmail.com>
2022-09-22 08:54:22 -0300

    Sep.22/2022

--- RunAcolite_Landsat.R
+++ RunAcolite_Landsat.R
@@ -2,9 +2,9 @@ rm(list=ls())
 gc()
 
 ##
-in.folder = "C:\\LC08_L1TP_014042_20160508_20200907_02_T1"
+in.folder = "C\\LC08_L1TP_014042_20160508_20200907_02_T1\\LC08_L1TP_014042_20160508_20200907_02_T1"
 out.folder = "C:\\Users\\cormi\\Documents\\test"
-out.settings = "C:\\Users\\cormi\\Documents\\acolite_py_win\\config\\defaultslandsat.txt"
+out.settings = "defaultslandsat_test1.txt"
 ##
 
 #Read in settings file
@@ -12,7 +12,8 @@ set.file = read.delim("C:\\Users\\cormi\\Documents\\acolite_py_win\\config\\defa
 #define input file
 set.file[3,1] = paste("inputfile=",in.folder,sep="")
 #define output file
-set.file[4,1] = paste("output=", out.folder,sep="")
+set.file[5,1] = paste("output=", out.folder,sep="")
+set.file[4,1] = "limit=25.663219,-79.324705,25.801764,-79.198116"
 #do a sunglint correction - seems this is all already in the file I have so no need to run code
 #set.file[33,1] = "glint_mask_rhos_band=1600"#change mask band
 #set.file[34,1] = "glint_mask_rhos_threshold=0.05"#change mask default of 0.05
@@ -25,14 +26,14 @@ set.file[4,1] = paste("output=", out.folder,sep="")
 
 #to fix error "ValueError: The requested sample points xi have dimension 6, but this RegularGridInterpolator has dimension 5" based on website help 
 #from https://odnature.naturalsciences.be/remsem/acolite-forum/viewtopic.php?t=321
-set.file[121,1]="dsf_interface_reflectance=True"
+set.file[98,1]="dsf_interface_reflectance=True"
 
 #write out the settings file
 write.table(set.file, paste(out.folder,"\\",out.settings,sep=""),row.names=F,col.names=F,quote=F )
 
 
 #Acolite call, have to provide the full paths for it to work
-cmd = paste("C:\\Users\\cormi\\Documents\\acolite_py_win\\acolite.exe --cli --settings=",out.folder,"\\",out.settings,sep="")
+cmd = paste("C:\\Users\\cormi\\Documents\\acolite_py_win\\dist\\acolite\\acolite.exe --cli --settings=",out.folder,"\\",out.settings,sep="")
 shell(cmd) #run acolite
 
 
