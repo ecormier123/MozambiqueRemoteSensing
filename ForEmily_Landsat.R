e4586fb03da338825b5f82af3532dcd9d7add671
ecormier123 <cormieremily@hotmail.com>
2022-11-07 10:10:40 -0400

    removed cluster, added new method of probability map

--- ForEmily_Landsat.R
+++ ForEmily_Landsat.R
@@ -1,3 +1,4 @@
+rm(list=ls())
 library("raster")
 library("caret")
 library("irr")
@@ -5,27 +6,29 @@ library("doParallel")  #Foreach Parallel Adaptor
 library("foreach")     #Provides foreach looping construct
 rasterOptions(maxmemory=7e+09,chunksize = 4e+08)
 
-setwd("C:\\Users\\cormi\\Documents\\test")
+setwd("C://Users//cormi//Documents//test//")
 
-#Read in raster dataset
-wv.dat = stack("Landsat8_Sept2016_copy.tif")
-names(wv.dat) = c("b","g","r","n", "depth","ndvi","gndvi")
-#depth.mask=wv.dat[[1]]#only used for blank raster to fill
-
-
+#Read in raster datasetwithout depth
+wv.dat2 = stack("Landsat8_Sept2016sansdepth_copy.tif")
+#wv.dat = stack("Landsat8_Sept2016_copy.tif")
+names(wv.dat2) = c("b","g","r","n","ndvi","gndvi")
+#names(wv.dat) = c("b","g","r","n", "depth","ndvi","gndvi")
+#remove NAs across board
+wv.dat <- mask(wv.dat2, calc(wv.dat2, fun = sum))
+depth.mask=wv.dat[[1]]#only used for blank raster to fill
+rm(wv.dat2)
 
 #Pick one training dataset
 train.dat = read.csv("C:\\Users\\cormi\\Documents\\test\\Trainingdata.csv")
 #traindat.shp = SpatialPointsDataFrame(coords=train.dat[,3:2],data=train.dat, proj4string=CRS(("+proj=longlat +datum=WGS84 +units=m +no_defs")))
 #train.dat2 <- spTransform(traindat.shp, crs(wv.dat))
+#train.dat3=extract(wv.dat,train.dat2, df=T)
 
 #
 list.bands.in = list( c("Class",     "b","g",  "r"))
 list.out.folder = c("./RF/")
 UseCores = detectCores()-1
 
-
-##
 wv.dat = wv.dat[[c( "b", "g" , "r" )]]
 wv.dat=getValues(wv.dat)
 id.na = which(!is.na(wv.dat[,1]))#gives index of true values
@@ -50,7 +53,8 @@ dir.create(paste(out.folder,"/cvrun",sep=""))
 
 #Define Model tuning
 ras.value = train.dat[,bands.in]
-ras.value[,4] = as.factor(ras.value[,4])#dependent value as a factor
+ras.value[,1] = as.factor(ras.value[,1])#dependent value as a factor
+ras.value = na.omit(ras.value)
 rfFit = train(form = Class ~ . , data = ras.value,
               method = "rf", tuneLength = (dim(ras.value)[2]-2),
               trControl = trainControl(method = "repeatedcv",number = 5,repeats = 10),
@@ -65,15 +69,18 @@ rm(rfFit)
 set.seed(805)
 trainIndex = createMultiFolds(ras.value[,1], k = 5, times = 10)#generate mutliplte data splits
 ###
-registerDoSEQ()#removes previous clusters
+#registerDoSEQ()#removes previous clusters
 #Define how many cores you want to use and Register CoreCluster
-cl  = makeCluster(UseCores)
-registerDoParallel(cl) 
+#cl  = makeCluster(UseCores)
+#registerDoParallel(cl) 
+}
 #
-foreach(i=1:50) %dopar% {
+#foreach(i=1:50) %dopar% {
+for(i in 1:50){
   library("raster")
   library("caret")
   library("irr")
+  i=2
   #Out data
   full.model = paste(out.folder, "cvrun/", "k",i, ".rds",sep="")
   exp.file = paste(out.folder, "cvrun/", "k",i,".csv",sep="")
@@ -82,7 +89,7 @@ foreach(i=1:50) %dopar% {
   ras.valueTrain = ras.value[ trainIndex[[i]],]
   ras.valueTest  = ras.value[-trainIndex[[i]],]
   rfFit.k = train(form = Class ~ . , data = ras.valueTrain,
-                  method = "rf", 
+                  method = "rf",
                   tuneGrid = data.frame(num.mtry),
                   trControl = trainControl(method = "none"),
                   verbose = FALSE)
@@ -91,9 +98,25 @@ foreach(i=1:50) %dopar% {
   ##
   set.seed(3)
   rfFit.final = predict(rfFit.k, ras.valueTest)
+  
   #Evaluate model
   full.dat=as.data.frame(cbind(rfFit.final,ras.valueTest[,1]))
-  conmat = table(full.dat)
+  
+  if (length(unique(full.dat[,1])) == length(unique(full.dat[,2]))){
+    conmat = table(full.dat)
+  }else{
+    conmat = table(full.dat)
+    miss.var = which(unique(full.dat[,2])%in%unique(full.dat[,1])==F)
+    conmat2=rbind(conmat, c(0,0,0))
+    rownames(conmat2) = c(rownames(conmat), miss.var)
+    fixind = order(row.names(conmat2), decreasing=F)
+    conmat2=conmat2[fixind,]
+    conmat=conmat2
+    rownames(conmat)=unique(full.dat[,2])
+    rm(conmat2,miss.var,fixind)
+  }
+}
+  
   conmat = apply(conmat, 2, function(x) as.numeric(as.character(x)))
   conkapp = kappa2(full.dat, weight = c("unweighted"))
   n = sum(conmat)#number of observations
@@ -113,23 +136,23 @@ foreach(i=1:50) %dopar% {
   out.dat.raster = raster(depth.mask)#define a raster
   out.dat = as.integer(out.dat)#define as integer to reduce file size
   out.dat.raster[id.na] =  out.dat#save output
-  out.dat.raster = out.dat.raster-1
+  #out.dat = out.dat-1
   writeRaster(out.dat.raster, out.ras, format="GTiff",NAflag = NaN,overwrite=T)
   ##
   rm(ras.valueTest,ras.valueTrain,exp.file,out.dat, out.ras, out.dat.raster)
     rm(rfFit.k, full.model)
   }
 #end cluster
-stopCluster(cl)
-registerDoSEQ()#removes previous clusters
-rm(cl)
+#stopCluster(cl)
+#registerDoSEQ()#removes previous clusters
+#rm(cl)
 gc()
 ###
 rm(num.mtry,trainIndex,ras.value)
 
 #Generate Mean confusion matrix statistics
 a = list.files(path=paste(out.folder, "/cvrun", sep=""), pattern=".csv")
-ab = paste(out.folder, "/cvrun",a,sep="")
+ab = paste(out.folder, "cvrun/",a,sep="")
 my.list = lapply(ab, read.csv)
 my.list  = lapply(my.list , as.matrix)
 rm(a,ab)
@@ -141,10 +164,36 @@ write.csv(final_mat,paste(out.folder, "CMsummary.csv", sep=""))
 rm(mat_rows,len_mat,vec,final_mat,my.list)
 ##
 
-##Read in all raster
+
+
+
+##Read in all raster2
 a = list.files(path=paste(out.folder, "/cvrun/", sep=""), pattern=".tif$")
-ab = paste(list.out.folder,out.folder, "/cvrun/",a,sep="")
+ab = paste(out.folder, "cvrun/",a,sep="")
 clas.dat = stack(ab)
+rm(a,ab)
+#
+beginCluster()
+#Modal
+getmode = function(v) {uniqv <- unique(v)
+uniqv[which.max(tabulate(match(v, uniqv)))]}
+mode.class.dat = clusterR(clas.dat, calc, args=list(fun=getmode))#Generate modal value 
+writeRaster(mode.class.dat,paste(out.folder, "modalmf.tif", sep=""),
+            format="GTiff",NAflag = NaN,overwrite=T) 
+endCluster()
+
+
+
+
+
+
+
+##Read in all raster
+a = list.files(path=paste(out.folder, "cvrun/", sep=""), pattern=".tif$")
+ab = paste(out.folder, "cvrun/",a,sep="")
+clas.dat = stack(ab)
+writeRaster(clas.dat, paste(out.folder, "kstack.tif", sep=""),
+            format="GTiff",NAflag = NaN,overwrite=T) 
 rm(a,ab)
 #
 beginCluster()
@@ -161,6 +210,6 @@ rm(clas.dat)
 rm(list = ls()[!ls() %in% c("wv.dat", "dep.val", "ndvi.thres","b.thres","gr","train.dat","depth.mask","id.na",
                             "list.bands.in","list.out.folder","UseCores")])
 removeTmpFiles(h=0)
-gc()
 
+gc()
 }
--- T20NQPreprocessingV2_Landsat.R
+++ T20NQPreprocessingV2_Landsat.R
@@ -146,3 +146,145 @@ writeRaster(dat.stack, write.data2, format="GTiff",NAflag = NaN, overwrite=T)
 
 
 
+##do without depth data----
+rm(list = ls())
+library("ncdf4")
+library("raster")
+library("rgdal")
+library("fields")
+library("snow")
+
+
+beginCluster()
+
+
+#Mask offshore depth NA NEED TO FIND/how to make?
+#offshore = shapefile("U:\\SatelliteData\\Fixed-20160913\\Processed\\ExtraFile\\Nullwaterdepth3_MultiPolygon_MultiPolygon.shp")
+#havetousefullimagewhenusingfullraster
+#import depth raster from NOAA
+depth = raster("C:\\Users\\cormi\\Documents\\test\\bathymetry\\Bathymetry_Bimini_NOAA.tif")
+#import created landmask
+land = raster("C:\\Users\\cormi\\Documents\\test\\landmask\\landmaskBimini.tif")
+
+#import satellite data nc file
+#nc.dat = "C:/Users/Cormi/Documents/test/subset_1_of_S2A_MSI_2021_06_14_07_56_37_T36KYU_L2R.nc"
+nc.dat ="C:\\Users\\cormi\\Documents\\test\\L8_OLI_2016_05_08_15_43_25_014042_L2R.nc"
+
+#create Output file
+write.data = "C:/Users/Cormi/Documents/test/Landsat8_Sept2016sansdepth.tif"
+write.data2 = "C:/Users/Cormi/Documents/test/Landsat8_Sept2016x10000sansdepth.tif"
+
+#createoutputfileforinverse
+#write.data = "C:/Users/Cormi/Documents/test/Landsat8_Sept2016_reverselandmask.tif"
+#write.data2 = "C:/Users/Cormi/Documents/test/Landsat8_Sept2016_reverselandmaskx10000.tif"
+
+
+####take out blue layer for extent matching and coordinate refrence systsem (crs) matching
+nc = nc_open(nc.dat)
+nc_atts <- ncatt_get(nc, 0)
+##
+blue1 = t(ncvar_get(nc, nc$var$rhos_483))
+#blue1 = t(ncvar_get(nc, nc$var$sza))
+blue = raster(blue1)
+##changeing projection with project4string
+proj4string(blue)  = crs(nc_atts$proj4_string)
+##clipping the blue to nc atts extent
+extent(blue) = c(nc_atts$xrange,nc_atts$yrange[2],nc_atts$yrange[1])
+
+#Depth Data 
+#project depth to same crs as blue
+depth = projectRaster(depth, blue, method="bilinear")
+
+##green
+###get rest of important bands to create final image
+green1 = t(ncvar_get(nc, nc$var$rhos_561))
+green = raster(green1)
+proj4string(green)  = crs(nc_atts$proj4_string)
+extent(green) = c(nc_atts$xrange,nc_atts$yrange[2],nc_atts$yrange[1])
+
+##red
+red1 = t(ncvar_get(nc, nc$var$rhos_655))
+red = raster(red1)
+proj4string(red)  = crs(nc_atts$proj4_string)
+extent(red) = c(nc_atts$xrange,nc_atts$yrange[2],nc_atts$yrange[1])
+
+##nir
+nir1 = t(ncvar_get(nc, nc$var$rhos_865))
+nir = raster(nir1)
+proj4string(nir)  = crs(nc_atts$proj4_string)
+extent(nir) = c(nc_atts$xrange,nc_atts$yrange[2],nc_atts$yrange[1])
+
+##stackalllayerstogether
+dat.stack = stack(blue,green,red,nir)
+
+##makeroom
+rm(blue,green,red,nir)
+
+##name the layers in data stack
+names(dat.stack) = c("blue","green","red","nir")
+
+###ifelse returns a value with the same shape as test which is filled with elements selected from either yes or no depending on whether the element of test is TRUE or FALSE."ifelse(test, yes, no)" 
+##testing to see if this works
+#landmask = t(ncvar_get(nc, nc$var$rhot_1614))
+#landmask = ifelse(landmask<="thresh.land",1,NA)#land mask finalized
+
+##cloud mask written by kristen
+cloud = t(ncvar_get(nc, nc$var$rhot_1373))
+cloud = ifelse(cloud<="thresh.cloud",1,NA)#cloud mask finalized
+
+neg.mask = ifelse(blue1<0 | green1<0 | red1<0 | nir1<0, NA, 1)#set to NA if any are negative
+
+#makeroom
+rm(blue1,green1,red1,nir1)
+##what do the empty square brackets do?
+cloud1 = dat.stack[[1]]
+cloud1[] = cloud
+neg.mask1 = dat.stack[[1]]
+neg.mask1[] = neg.mask
+
+##create final mask
+rmaskrs = cloud1*neg.mask1*land
+plot(rmaskrs)
+
+##remove deep water
+offshore=depth<=-30
+plot(offshore)
+#remove 0 values
+offshoremask <- clamp(offshore, lower=1, useValues=FALSE)
+
+#makeroom
+rm(cloud,landmask1,neg.mask,cloud1,neg.mask1)
+
+
+
+#maskdat.stack and crop
+dat.stack = mask(x=dat.stack, mask = rmaskrs, inverse=T)#land, cloud, neg mask
+#mask dat.stack with offshore
+dat.stack = mask(x=dat.stack, mask = offshoremask, inverse=T)
+
+#to create reverse land mask
+#dat.stack = mask(x=dat.stack, mask = rmaskrs)
+
+##
+nc_close(nc)
+#makeroom
+rm(nc,nc.dat,nc_atts,rmaskrs,offshoremask)
+
+##createndvilayer
+ndvi = (dat.stack$nir-dat.stack$red)/(dat.stack$nir+dat.stack$red)
+
+##what is gndvi?
+gndvi = (dat.stack$nir-dat.stack$green)/(dat.stack$nir+dat.stack$green)
+
+#create stack with ndvi and gndvi
+dat.stack = stack(dat.stack,ndvi,gndvi)
+rm(ndvi,gndvi)
+
+##name layers in dat stack
+names(dat.stack) = c("blue","green","red","nir", "ndvi","gndvi")
+
+##make the final raster!
+writeRaster(dat.stack, write.data, format="GTiff",NAflag = NaN, overwrite=T)
+
+##
+
